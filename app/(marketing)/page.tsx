7addb499196d9f541fb982fc300b4e2a6eb089ab
Nikhildeveloper06 <itsniktripathi.07@gmail.com>
2026-07-07 17:39:04 +0530

    chore: remove promo banner

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,25 +1,21 @@
 import { Hero } from "@/components/home/hero";
-import { TickerBanner } from "@/components/home/ticker-banner";
 import { StatsBand } from "@/components/home/stats-band";
 import { ExperienceShowcase } from "@/components/home/experience-showcase";
 import { ServicesGrid } from "@/components/home/services-grid";
 import { HowItWorks } from "@/components/home/how-it-works";
 import { PricingPreview } from "@/components/home/pricing-preview";
 import { ReviewsMarquee } from "@/components/home/reviews-marquee";
-import { PromoBanner } from "@/components/home/promo-banner";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <TickerBanner />
       <StatsBand />
       <ExperienceShowcase />
       <ServicesGrid />
       <HowItWorks />
       <PricingPreview />
       <ReviewsMarquee />
-      <PromoBanner />
     </>
   );
 }
